905428cc53b37c86f650f00afc5df3d6466d628d
Swimming Failure <swimmingfailure@gmail.com>
2021-06-04 00:58:23 -0400

    templates for nav bar and footer

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ $(function() {
     console.log("!!!!!!!!!!!!!!!!");
     console.log("SWIMMING FAILURE");
     console.log("!!!!!!!!!!!!!!!!");
+    $("#nav").load("nav.html");
+    $("#footer").load("footer.html");
 });
