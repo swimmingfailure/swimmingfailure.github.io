b3e53a68ceb2245ccd0a4607dc7773245f5f57f7
Swimming Failure <swimmingfailure@gmail.com>
2021-06-08 19:24:30 -0400

    POC of play state tracking

--- script/music.js
+++ script/music.js
@@ -6,21 +6,36 @@ AUDIO_ELEM = document.getElementById("audio");
 $(function() {
     console.log("MUSIC");
     $.getJSON(JSON_FILEPATH, function(json) {
-        for (let i in json) {
-            let filepath = MUSIC_FOLDERPATH + json[i].title.toLowerCase().replace(/\s/g, '').replace(/'/g, '');
-            let html = 
-            `<div class="track" data-track="${filepath}.mp3" style="background-image: url(${filepath}.jpg)">
-                <p>${json[i].title}</p>
-                <p>${json[i].artist}</p>
-                <p>${json[i].date}</p>
-            </div>`
-
-            $(TRACKS_DIV).append(html);
-        }
-
+        makeTrackElems(json);
         $(".track").click(function() {
-            AUDIO_ELEM.src = $(this).data("track");
-            AUDIO_ELEM.play();
+            let parser = document.createElement('a');
+            parser.href = AUDIO_ELEM.src;
+            // Switch tracks when different track is clicked
+            if (parser.pathname != $(this).data("track")) {
+                AUDIO_ELEM.src = $(this).data("track");
+                AUDIO_ELEM.play();
+            // Play clicked track if it is paused
+            } else if (AUDIO_ELEM.paused) {
+                AUDIO_ELEM.play();
+            // Pause clicked track if it is playing
+            } else {
+                AUDIO_ELEM.pause();
+            }
         });
     });
 });
+
+function makeTrackElems(json) {
+    for (let i in json) {
+        let filepath = MUSIC_FOLDERPATH + json[i].title.toLowerCase().replace(/\s/g, '').replace(/'/g, '');
+        
+        let html = 
+        `<div class="track" data-track="${filepath}.mp3" style="background-image: url(${filepath}.jpg)">
+            <p>${json[i].title}</p>
+            <p>${json[i].artist}</p>
+            <p>${json[i].date}</p>
+        </div>`
+
+        $(TRACKS_DIV).append(html);
+    }
+}
