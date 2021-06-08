68748865096fd87d699b14d98363586a33fcedae
Swimming Failure <swimmingfailure@gmail.com>
2021-06-08 03:24:28 -0400

    POC of dynamic track list

--- script/music.js
+++ script/music.js
@@ -4,6 +4,23 @@ $(function() {
         for (let i in json) {
             let title = json[i].title;
             let artist = json[i].artist;
+            let date = json[i].date;
+            let file = title.toLowerCase().replace(/\s/g, '');
+
+            $("#tracks").append(`
+            <div class="track" data-file="${file}">
+                <p>${title}</p>
+                <p>${artist}</p>
+                <p>${date}</p>
+            </div>
+            `);
         }
+
+        $(".track").click(function() {
+            let mp3 = "/music/" + $(this).data("file") + ".mp3";
+            $("#audio").attr("src", mp3);
+            $(this).attr("background", "blue");
+            document.getElementById("audio").play();
+        });
     });
 });
