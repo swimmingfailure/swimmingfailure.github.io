9b3c87e1c3b3a465974048dd67ad43c1c49705d8
Swimming Failure <swimmingfailure@gmail.com>
2021-06-08 03:56:25 -0400

    POC of multiple playable, album covered tracks

--- script/music.js
+++ script/music.js
@@ -2,14 +2,14 @@ $(function() {
     console.log("MUSIC");
     $.getJSON('/music/music.json', function(json) {
         for (let i in json) {
-            let filepath = "/music/" + json[i].title.toLowerCase().replace(/\s/g, '');
-            let html = `
-            <div class="track" data-mp3="${filepath}.mp3" style="background-image: url(${filepath}.jpg)">
+            let filepath = "/music/" + json[i].title.toLowerCase().replace(/\s/g, '').replace(/'/g, '');
+            let html = 
+            `<div class="track" data-mp3="${filepath}.mp3" style="background-image: url(${filepath}.jpg)">
                 <p>${json[i].title}</p>
                 <p>${json[i].artis}</p>
                 <p>${json[i].date}</p>
             </div>`
-            
+
             $("#tracks").append(html);
         }
 
