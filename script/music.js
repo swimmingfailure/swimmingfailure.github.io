edc1badba53349508595aaf0ac6da094ada02e26
Swimming Failure <swimmingfailure@gmail.com>
2021-06-08 04:14:41 -0400

    global vars for track player to make reusable, plus a little bit of track styling

--- script/music.js
+++ script/music.js
@@ -1,22 +1,26 @@
+JSON_FILEPATH = "/music/music.json";
+MUSIC_FOLDERPATH = "/music/";
+TRACKS_DIV = "#tracks";
+AUDIO_ELEM = document.getElementById("audio");
+
 $(function() {
     console.log("MUSIC");
-    $.getJSON('/music/music.json', function(json) {
+    $.getJSON(JSON_FILEPATH, function(json) {
         for (let i in json) {
-            let filepath = "/music/" + json[i].title.toLowerCase().replace(/\s/g, '').replace(/'/g, '');
+            let filepath = MUSIC_FOLDERPATH + json[i].title.toLowerCase().replace(/\s/g, '').replace(/'/g, '');
             let html = 
-            `<div class="track" data-mp3="${filepath}.mp3" style="background-image: url(${filepath}.jpg)">
+            `<div class="track" data-track="${filepath}.mp3" style="background-image: url(${filepath}.jpg)">
                 <p>${json[i].title}</p>
-                <p>${json[i].artis}</p>
+                <p>${json[i].artist}</p>
                 <p>${json[i].date}</p>
             </div>`
 
-            $("#tracks").append(html);
+            $(TRACKS_DIV).append(html);
         }
 
         $(".track").click(function() {
-            let mp3 = $(this).data("mp3");
-            $("#audio").attr("src", mp3);
-            document.getElementById("audio").play();
+            AUDIO_ELEM.src = $(this).data("track");
+            AUDIO_ELEM.play();
         });
     });
 });
