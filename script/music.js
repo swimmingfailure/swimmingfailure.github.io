a99ba4be94d29e0f9f04724bb39e6a0ff86c1430
Swimming Failure <swimmingfailure@gmail.com>
2021-06-08 19:50:07 -0400

    basic visual play state tracking

--- script/music.js
+++ script/music.js
@@ -12,14 +12,22 @@ $(function() {
             parser.href = AUDIO_ELEM.src;
             // Switch tracks when different track is clicked
             if (parser.pathname != $(this).data("track")) {
+                $(`[data-track="${parser.pathname}"]`).css({"outline": "0px",
+                                                            "outline-offset": "0px"});
                 AUDIO_ELEM.src = $(this).data("track");
                 AUDIO_ELEM.play();
+                $(this).css({"outline": "solid var(--white) 6px",
+                             "outline-offset": "-5px"});
             // Play clicked track if it is paused
             } else if (AUDIO_ELEM.paused) {
                 AUDIO_ELEM.play();
+                $(this).css({"outline": "solid var(--white) 6px",
+                             "outline-offset": "-5px"});
             // Pause clicked track if it is playing
             } else {
                 AUDIO_ELEM.pause();
+                $(this).css({"outline": "solid var(--gray) 6px",
+                             "outline-offset": "-5px"});
             }
         });
     });
